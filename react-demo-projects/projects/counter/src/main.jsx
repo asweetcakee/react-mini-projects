f7996ec1216e4f3cd5253a3f9ebe8ee5083a5ab8
asweetcake <beka.murat.2021@gmail.com>
2025-10-18 21:03:20 +0500

    chore: integrate @ui-theme into counter app and enable shared CSS variables

--- react-demo-projects/projects/counter/src/main.jsx
+++ react-demo-projects/projects/counter/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Counter from './Counter.jsx'
+import "@ui/ui-theme"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
